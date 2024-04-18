92a3a44f8cb3b808b8129aad9586ccbd50fea5d3
michaelschuff <mschuff0881@gmail.com>
2024-04-17 20:06:12 -0400

    Added persistency of created channels through bot lifespans

--- index.js
+++ index.js
@@ -36,10 +36,46 @@ for (const file of eventFiles) {
 	}
 }
 
-global.utcVCs = new Map()
-global.VCGenerators = new Map()
-global.tempVCs = new Map()
-async function updateUTCVoiceChannel() {
+
+fs.readFile("global.json", (error, data) => {
+    if (error) {
+        global.utcVCs = new Map()
+        global.VCGenerators = new Map()
+        global.tempVCs = new Map()
+    } else {
+        const parsedData = JSON.parse(data, reviver);
+
+        global.utcVCs = parsedData.utcVCs;
+        global.VCGenerators = parsedData.VCGenerators;
+        global.tempVCs = parsedData.tempVCs;
+    }
+    
+})
+ 
+process.env.DEBUG == "true" ? client.login(devToken) : client.login(prodToken);
+
+
+function replacer(key, value) {
+    if(value instanceof Map) {
+        return {
+            dataType: 'Map',
+            value: Array.from(value.entries()), // or with spread: value: [...value]
+        };
+    } else {
+        return value;
+    }
+}
+
+function reviver(key, value) {
+    if(typeof value === 'object' && value !== null) {
+        if (value.dataType === 'Map') {
+            return new Map(value.value);
+        }
+    }
+    return value;
+}
+
+async function update() {
     const currentTime = new Date().toUTCString().substring(17,22)
     for (guild of global.utcVCs.entries()) {
         for (channel of guild[1]) {
@@ -47,7 +83,24 @@ async function updateUTCVoiceChannel() {
             .catch(console.error);
         }
     }
-}
-setInterval(updateUTCVoiceChannel, 60*5*1000); // 5 minutes worth of milliseconds
 
-process.env.DEBUG == "true" ? client.login(devToken) : client.login(prodToken);
+    const obj = {
+        utcVCs: global.utcVCs,
+        VCGenerators: global.VCGenerators,
+        tempVCs: global.tempVCs,
+    }
+    const data = JSON.stringify(obj, replacer)
+    
+    fs.writeFile("global.json", data, (error) => {
+    if (error) {
+        console.error(error);
+        throw error;
+    }
+
+    console.log("data.json written correctly");
+    console.log(global.VCGenerators)
+});
+
+
+}
+setInterval(update, 10*1000); // 5 minutes worth of milliseconds
