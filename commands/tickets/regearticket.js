e3d7dfa1af0670e77e24ceee81b990f84a169886
michaelschuff <mschuff0881@gmail.com>
2024-04-20 15:02:19 -0400

    Added quick and dirty regear ticket system for terry reset today

--- commands/tickets/regearticket.js
+++ commands/tickets/regearticket.js
@@ -1,37 +1,31 @@
 const { ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, PermissionFlagsBits, ChannelType, PermissionsBitField  } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('registrationticketgen')
-        .setDescription('This command creates a registration ticket message in the channel it is used in')
+        .setName('regearticketgen')
+        .setDescription('This command creates a regear ticket message in the channel it is used in')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator) // only admins can use this command
         .setDMPermission(false) // cant create voice chats in dms
         .addRoleOption(option =>
             option.setName('role')
                 .setDescription('What role handles these tickets?')),
-        // .addStringOption(option =>
-        //     option.setName('Title')
-        //         .setDescription('Title to display'))
-        // .addStringOption(option =>
-        //     option.setName('Description')
-        //         .setDescription('Descripitiony')),
     async execute(interaction) {
         const reply = await interaction.reply({
           content: '🫡',
           ephemeral: true
         });
         const role = interaction.options.getRole('role');
-        const applyButton = new ButtonBuilder()
-            .setCustomId('apply now')
-            .setLabel('Apply Now')
-            .setStyle(ButtonStyle.Success);
+        const regearButton = new ButtonBuilder()
+            .setCustomId('regear request')
+            .setLabel('Request Regear')
+            .setStyle(ButtonStyle.Primary);
         
         const row = new ActionRowBuilder()
-			.addComponents(applyButton);
+			.addComponents(regearButton);
 
 
         const applicationEmbed = new EmbedBuilder()
             .setColor(0xFFA500)
-            .setTitle('Open a ticket to apply to Gridlocke!')
+            .setTitle('Open a ticket to request your regear')
             .setDescription('Click the button to open a ticket')
         
         
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,6 +5,10 @@ module.exports = {
 	async execute(interaction) {
         if (interaction.isButton()) {
             if (interaction.customId == 'apply now') {
+                await interaction.reply({
+                  content: '🫡',
+                  ephemeral: true
+                });
                 var category = interaction.guild.channels.cache.find(channel => channel.type == ChannelType.GuildCategory && channel.name == "applications");
                 if (!category) {
                     category = await interaction.guild.channels.create({
@@ -31,6 +35,46 @@ module.exports = {
                     .setTitle('Please answer the following questions')
                     .setDescription('- What brought you to Gridlocke?\n- What are your in-game main role ?\n- What is your main playtime in UTC?\n- Do you play on PC, Mobile or both?\n- Are you at least 18 Y/O?\n- Do you play any other online games?\n- Please type your IGN:\n- Please include a screenshot of your login screen/Character stats')
                 
+                
+                
+                await channel.send({ embeds: [applicationEmbed], components: [row] });
+                await channel.send("<@" + interaction.member.user.id + ">");
+                if (!(global.ticketGenerators.get(interaction.message.id) === undefined)) {
+                    await channel.send("<@&" + global.ticketGenerators.get(interaction.message.id) + ">");
+                }
+            }
+
+            if (interaction.customId == 'regear request') {
+                await interaction.reply({
+                  content: '🫡',
+                  ephemeral: true
+                });
+                var category = interaction.guild.channels.cache.find(channel => channel.name == "regears" && channel.type == ChannelType.GuildCategory);
+                if (!category) {
+                    category = await interaction.guild.channels.create({
+                        name: "regears", 
+                        type: ChannelType.GuildCategory,
+                    });
+                }
+                const channel = await category.children.create({
+                    name: interaction.member.user.username + "'s regear'",
+                    type: ChannelType.GuildText
+                })
+
+                const applyButton = new ButtonBuilder()
+                    .setCustomId('close ticket')
+                    .setLabel('Close Ticket')
+                    .setStyle(ButtonStyle.Danger);
+                
+                const row = new ActionRowBuilder()
+                    .addComponents(applyButton);
+
+
+                const applicationEmbed = new EmbedBuilder()
+                    .setColor(0xFFA500)
+                    .setTitle('Please post the following to receive your regear')
+                    .setDescription('- Date and time of the content you died in\n- Name of the caller running the content\n- Screenshot of your death (see example)')
+                    .setImage('https://media.discordapp.net/attachments/1231318288638803988/1231318321803169893/RegearDeathExample.png?ex=6636858c&is=6624108c&hm=14506cfb212001ac037935b1407afc32af91e4e7dfe0a26cb9196e4704a4c624&=&format=webp&quality=lossless&width=1384&height=1060')
                 
                 
                 await channel.send({ embeds: [applicationEmbed], components: [row] });
