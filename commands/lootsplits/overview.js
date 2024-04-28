ac2c5cd078adf382ca1d7cd3a95845a16a800f1c
michaelschuff <mschuff0881@gmail.com>
2024-04-28 00:26:33 -0400

    added overview command

---
+++ commands/lootsplits/overview.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const { SaveData, getGuildData, setGuildData } = require("./../../database/loader.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('overview')
+        .setDescription('View all current balances')
+        .setDMPermission(false),
+    async execute(interaction) {
+        var guildData = getGuildData(interaction.guild.id);
+        if (interaction.member.roles.cache.some(r => r.name === guildData.lootSplitOfficerRoleName)) {
+            if (guildData.commandLogId != -1) {
+                const channel = await interaction.guild.channels.fetch(guildData.commandLogId);
+                channel.send(interaction.member.displayName + " used /overview ");
+            }
+            const balances = guildData.userBalances;
+            var str = ``;
+            balances.forEach((value, key, map) => {
+                str += `<@${key}>:  ${value}\n`;
+            })
+            const reply = await interaction.reply({
+                content: str,
+                ephemeral: true
+            });
+            
+            // setGuildData(interaction.guildId, guildData);
+            // SaveData();
+        } else {
+            await interaction.reply({
+                content: 'You are not a loot split officer',
+                ephemeral: true
+            });
+        }
+    }
+};
